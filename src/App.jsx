12095601c0cc2293978673eca4bc501eaf709874
QISUN000 <qsun4@rrc.ca>
2024-09-05 19:13:22 -0500

    2

--- src/App.jsx
+++ src/App.jsx
@@ -32,8 +32,8 @@ function App() {
       <div className='lg:hidden duration-500 block absolute top-32 left-0 right-0 bg-gray-50 transition-opacity text-black'>
         <ul className='text-center text-2xl p-20'>
           <Link spy={true} smooth={true} className='my-4 py-4 block' to="Services"><li>Services</li></Link>
+          <Link spy={true} smooth={true} className='my-4 py-4 block' to="Creations"><li>Creations</li></Link>
           <Link spy={true} smooth={true} className='my-4 py-4 block' to="Contacts"><li>Contacts</li></Link>
-          <Link spy={true} smooth={true} className='my-4 py-4 block' to="Locations"><li>Locations</li></Link>
         </ul>
       </div>
     </>
@@ -52,8 +52,8 @@ function App() {
               <Link className="transition ease-in-out delay-150 hover:cursor-pointer hover:scale-110 border-b-2 border-transparent hover:border-current" spy={true} smooth={true} to="Services"><li>Services
 
               </li></Link>
+              <Link className="transition ease-in-out delay-150 hover:cursor-pointer hover:scale-110 border-b-2 border-transparent hover:border-current" spy={true} smooth={true} to="Creations"><li>Creations</li></Link>
               <Link className="transition ease-in-out delay-150 hover:cursor-pointer hover:scale-110 border-b-2 border-transparent hover:border-current" spy={true} smooth={true} to="Contacts"><li>Contacts</li></Link>
-              <Link className="transition ease-in-out delay-150 hover:cursor-pointer hover:scale-110 border-b-2 border-transparent hover:border-current" spy={true} smooth={true} to="Locations"><li>Locations</li></Link>
             </ul>
 
           </div>
@@ -83,12 +83,12 @@ function App() {
 
       {/* Services Section */}
       <section name="Services" className="py-16 mx-auto w-10/12 ">
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-12  lg:gap-0">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-12  gap-6 lg:gap-0">
           {/* Background lines - visible only on large screens */}
           <div className="hidden lg:block w-px lg:col-start-4 lg:row-start-1 lg:row-end-5 bg-gray-200 "></div>
           <div className="hidden lg:block w-px lg:col-end-9 lg:row-start-2 lg:row-end-6 bg-gray-200 "></div>
 
-        
+
           <div data-aos="fade-up" className='lg:col-end-11 lg:col-span-3 flex flex-col items-start justify-center z-0  lg:mb-10  '>
             <h2 className="text-4xl lg:text-5xl mb-2 z-10">Our Services</h2>
             <p className="text-lg lg:text-xl text-gray-600  z-10">Indulge and Shine</p>
@@ -111,7 +111,7 @@ function App() {
               <button className="border border-black px-4 py-1 rounded-full hover:bg-black hover:text-white transition duration-300">Menu →</button>
             </div>
           </div>
-          
+
 
           {/* Coffee */}
           <div data-aos="fade-up" className='lg:col-end-7 lg:col-span-3 lg:row-start-4 font-normal'>
@@ -140,7 +140,7 @@ function App() {
       </section>
 
       {/* Our Creations */}
-      <div className='mt-28'>
+      <div className='mt-28' name="Creations">
         <div className='w-10/12 flex mx-auto items-center justify-between'>
           <div>
             <h2 className="text-5xl mb-2 relative z-10">Our Creations</h2>
@@ -155,21 +155,21 @@ function App() {
 
       {/* plan you visit */}
       <div className="mx-auto flex w-full md:w-3/5 lg:w-2/5 flex-col items-center mt-24 mb-16 lg:mt-56 lg:mb-28 relative px-4">
-  <div className="absolute z-10 top-[-60px] lg:top-[-90px] text-center">
-    <p className="text-lg md:text-xl">Sip. Relax. Beautify.</p>
-    <h2 className="text-6xl lg:text-7xl xl:text-8xl">Nails Salons</h2>
-  </div>
-  <img src={Image2} alt="Pedicure" className="object-cover w-3/4 md:w-2/3 lg:w-1/2" />
-  <div className="py-6 md:py-10 flex-col flex justify-center items-center">
-    <p className="text-lg md:text-xl text-center">Get yourself 15% Discount and<br /> a FREE Cocktail on the first visit,<br /> when you book online.</p>
-    <button className="mt-4 text-lg md:text-2xl border border-black px-6 py-3 md:px-8 md:py-4 rounded-full hover:bg-black hover:text-white transition duration-300">
-      PLAN YOUR VISIT
-    </button>
-  </div>
-</div>
+        <div className="absolute z-10 top-[-60px] lg:top-[-90px] text-center">
+          <p className="text-lg md:text-xl">Sip. Relax. Beautify.</p>
+          <h2 className="text-6xl lg:text-7xl xl:text-8xl">Nails Salons</h2>
+        </div>
+        <img src={Image2} alt="Pedicure" className="object-cover w-3/4 md:w-2/3 lg:w-1/2" />
+        <div className="py-6 md:py-10 flex-col flex justify-center items-center">
+          <p className="text-lg md:text-xl text-center">Get yourself 15% Discount and<br /> a FREE Cocktail on the first visit,<br /> when you book online.</p>
+          <button className="mt-4 text-lg md:text-2xl border border-black px-6 py-3 md:px-8 md:py-4 rounded-full hover:bg-black hover:text-white transition duration-300">
+            PLAN YOUR VISIT
+          </button>
+        </div>
+      </div>
 
-      {/* footer */}  
-      <div className='w-10/12 border-t border-gray-200 py-20 mx-auto'>
+      {/* footer */}
+      <div className='w-10/12 border-t border-gray-200 py-20 mx-auto' name="Contacts">
         <div className='grid grid-cols-1 sm:grid-cols-5 gap-4'>
           <div className='sm:col-start-2 sm:col-span-1 mb-6 sm:mb-0'>
             <h2 className='font-medium text-2xl mb-2'>Contacts</h2>
